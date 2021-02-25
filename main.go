0af32e62d7b1b4664c77a8e9d18cfe673f44c8b1
Vedran Stankovic <contact@vedrans.me>
2021-02-25 20:13:12 +0000

    minimum fn

--- main.go
+++ main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+	"sort"
+
 	cli "github.com/urfave/cli/v2"
 )
 
 func main() {
 
-	_ = &cli.App{
+	app := &cli.App{
 		Name: "isecode",
 		Usage: "utility cli tool to populate your code with traceable and secure error codes",
 	}
 
+	app.UseShortOptionHandling = true
+
+	app.Action = func(c *cli.Context) error {
+		fmt.Println("Run with -h to see list of available commands")
+		return nil
+	}
+
+	sort.Sort(cli.FlagsByName(app.Flags))
+	sort.Sort(cli.CommandsByName(app.Commands))
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 }
